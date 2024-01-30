06987533006e9ad9af6d422e15e11b629c24d105
Ítalo de Azevedo Albuquerque <italodeazevedo99@gmail.com>
2024-01-30 20:50:56 -0300

    git init

---
+++ src/main/java/com/italodea/pressureMonitor/PressureMonitor/PressureMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.italodea.pressureMonitor.PressureMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PressureMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PressureMonitorApplication.class, args);
+	}
+
+}
