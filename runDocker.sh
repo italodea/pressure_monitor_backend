4edd71251817b12cab435c6d4c6602f9214502dd
Ítalo de Azevedo Albuquerque <italodeazevedo99@gmail.com>
2024-02-09 09:19:00 -0300

    pressures

---
+++ runDocker.sh
@@ -0,0 +1,2 @@
+/usr/bin/env /home/italo/.vscode/extensions/redhat.java-1.26.0-linux-x64/jre/17.0.9-linux-x86_64/bin/java @/tmp/cp_3itllopdgigs7c6v0zc3txt8e.argfile com.italodea.pressureMonitor.PressureMonitor.PressureMonitorApplication  cd /home/italo/PressureMonitor ;
+/usr/bin/env /home/italo/.vscode/extensions/redhat.java-1.26.0-linux-x64/jre/17.0.9-linux-x86_64/bin/java @/tmp/cp_3itllopdgigs7c6v0zc3txt8e.argfile com.italodea.pressureMonitor.PressureMonitor.PressureMonitorApplication 
